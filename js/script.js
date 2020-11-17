453400f406340f57e1bd6dc787f896088e63bacd
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-17 16:57:16 +0100

    array image

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Ricreare lo slider di immagini con Vue
 
+var app = new Vue({
+  el: "#app",
+  data: {
+    imageIndex : 0,
+    image : ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg"]
+  }
+})
+

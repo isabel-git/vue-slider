13557b9570d2805880cc30d182831214b409745d
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-17 17:13:17 +0100

    methods nextImage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ var app = new Vue({
   data: {
     imageIndex : 0,
     image : ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg"]
+  },
+  methods: {
+    nextImage: function () {
+      this.imageIndex++
+
+      if (this.imageIndex === (this.image.length - 1)) {
+        this.imageIndex = 0
+      }
+      
+
+        
+      }
+
+
   }
+
+  
 })
 
